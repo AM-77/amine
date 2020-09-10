df37697fcef40d65316ff3b9b4941ad808829a03
AM-77 <amine.griche77@protonmail.ch>
2020-09-09 20:41:20 -0500

    personl site

---
+++ media/data/old-works.js
@@ -0,0 +1,79 @@
+const old_works = [
+  {
+    "title": "realestate",
+    "desc": "A web platform for selling realestate in Algeria.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/realestate"
+    },
+    "tags": ["Java", "SpringBoot", "MySQL", "thymeleaf"]
+  },
+  {
+    "title": "slicarousel",
+    "desc": "A fully responsive jQuery carousel plugin.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/slicarousel",
+      "Demo": "https://slicarousel.medamine.now.sh/"
+    },
+    "tags": ["JavaScript", "jQuery"]
+  },
+  {
+    "title": "dark-github-theme",
+    "desc": "A dark theme for the github website.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/dark-github-theme"
+    },
+    "tags": ["CSS"]
+  },
+  {
+    "title": "form-validator.js",
+    "desc": "A lightweight JavaScript form validation library.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/form-validator.js"
+    },
+    "tags": ["JavaScript"]
+  },
+  {
+    "title": "sooner",
+    "desc": "A template for a countdown timer website for launching a biz.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/HTLM5-Templates/tree/master/Sooner",
+      "Link": "https://sooner.medamine.now.sh/"
+    },
+    "tags": ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    "title": "Amir",
+    "desc": "A template for a portfolio website.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/HTLM5-Templates/tree/master/Amir",
+      "Link": "https://amir.am77.now.sh/"
+    },
+    "tags": ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    "title": "Bread Co.",
+    "desc": "A template for a Bakery website.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/HTLM5-Templates/tree/master/Bread Co.",
+      "Link": "https://bread-co.am77.now.sh/"
+    },
+    "tags": ["HTML", "CSS", "JavaScript", "Materialize"]
+  },
+  {
+    "title": "webrtc-chat",
+    "desc": "A webrtc video, audio and messages chat application. - peer 2 peer -",
+    "links": {
+      "Source Code": "https://github.com/AM-77/webrtc-chat"
+    },
+    "tags": ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    "title": "University Ibn Khaldoun Community",
+    "desc": "A landing page for the community of the University Ibn Khaldoun .",
+    "links": {
+      "Source Code": "https://github.com/AM-77/UIKCommunity.github.io.git",
+      "Link": "https://uik-community.vercel.app"
+    },
+    "tags": ["HTML", "CSS", "JavaScript"]
+  }
+]
---
+++ media/data/works.js
@@ -0,0 +1,173 @@
+const works = [
+  {
+    "title": "Nyno-UI",
+    "desc": "A complete CSS framework based on flexbox.",
+    "links": {
+      "Source Code": "https://github.com/AM77-Dev/Nyno-UI",
+      "Documentation": "https://docs.medamine.now.sh/"
+    },
+    "tags": ["CSS", "SCSS"]
+  },
+  {
+    "title": "react-datepickerrz",
+    "desc": "With double r & z, a customizable, zero-dependency datepicker component for React",
+    "links": {
+      "Source Code": "https://github.com/AM-77/react-datepickerrz",
+      "Demo": "https://am-77.github.io/react-datepickerrz/"
+    },
+    "tags": ["React", "JavaScript"]
+  },
+  {
+    "title": "react-coder",
+    "desc": "A lightweight, reusable code wrapper & editor component for react.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/react-coder",
+      "Documentation": "https://am-77.github.io/react-coder/"
+    },
+    "tags": ["React", "TypeScript"]
+  },
+  {
+    "title": "react-floating-buttons",
+    "desc": "A lightweight (zero-dependency), customizable and animated react floating buttons",
+    "links": {
+      "Source Code": "https://github.com/AM-77/react-floating-buttons",
+      "Demo": "https://am-77.github.io/react-floating-buttons-demo/"
+    },
+    "tags": ["React", "JavaScript"]
+  },
+  {
+    "title": "deno-dotenv",
+    "desc": "A zero-dependency deno module that loads environment variables from a file.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/deno-dotenv",
+      "Article": "https://am77-blog.netlify.app/build-your-own-deno-dotenv/"
+    },
+    "tags": ["Deno", "TypeScript"]
+  },
+  {
+    "title": "deno-rest-api",
+    "desc": "A rest API made with Deno.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/deno-rest-api",
+      "Article": "https://dev.to/am77/deno-v1-0-303j"
+    },
+    "tags": ["Deno", "TypeScript"]
+  },
+  {
+    "title": "p-gen",
+    "desc": "A lightweight, customizable javascript password generator.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/p-gen",
+      "Demo": "https://p-gen.vercel.app/"
+    },
+    "tags": ["JavaScript"]
+  },
+  {
+    "title": "cli-coloring",
+    "desc": "A text coloring and formatting for the nodejs console logs.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/cli-coloring"
+    },
+    "tags": ["JavaScript"]
+  },
+  {
+    "title": "dz-communes",
+    "desc": "A nodejs module for the list of all of the 48 wilayas of Algeria and their communes.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/dz-communes",
+      "Demo": "https://dz-communes.herokuapp.com/"
+    },
+    "tags": ["JavaScript"]
+  },
+  {
+    "title": "el-mesa",
+    "desc": "A peer to peer chat room with react, node, express and socket.io,",
+    "links": {
+      "Client Side": "https://github.com/AM-77/el-mesa",
+      "Server Side": "https://github.com/AM-77/el-mesa-server",
+      "Link": "https://el-mesa.netlify.app/"
+    },
+    "tags": ["Node", "Express", "SocketIO", "React", "JavaScript"]
+  },
+  {
+    "title": "lil-urls",
+    "desc": "A Custom URL Shortener, with node, express, sqlite3, typescript & react.",
+    "links": {
+      "Client Side": "https://github.com/AM-77/lil-urls",
+      "Server Side": "https://github.com/AM-77/lil-urls-server",
+      "Link": "https://lil-urls.vercel.app/"
+    },
+    "tags": ["Node", "Express", "SQLite3", "React", "JavaScript"]
+  },
+  {
+    "title": "savednotes",
+    "desc": "A web app to save & share notes. w/ node, express, react & mySql.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/savednotes"
+    },
+    "tags": ["Node", "Express", "MySQL", "React", "TypeScript"]
+  },
+  {
+    "title": "books-exchange (WIP)",
+    "desc": "An Algerian books exchanging plateform",
+    "links": {
+      "Source Code": "https://github.com/AM-77/books-exchange"
+    },
+    "tags": ["Node", "Express", "MongoDB", "React", "JavaScript"]
+  },
+  {
+    "title": "SOACC",
+    "desc": "An inscription management app for the university and the library - MicroServices -",
+    "links": {
+      "Source Code": "https://github.com/AM-77/SOACC"
+    },
+    "tags": ["Node", "Express", "MongoDB", "React"]
+  },
+  {
+    "title": "letra",
+    "desc": "A Lyrics application.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/letra",
+      "Link": "https://am-77.github.io/letra/#/"
+    },
+    "tags": ["Node", "Express", "React", "JavaScript"]
+  },
+  {
+    "title": "border-radius-generator",
+    "desc": "A border-radius generator with react.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/border-radius-generator",
+      "Link": "https://am-77.github.io/border-radius-generator/"
+    },
+    "tags": ["React", "TypeScript"]
+  },
+  {
+    "title": "DET",
+    "desc": "'Distribution Des Equipments' A web app for selling industrial equipments.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/DET",
+      "Link": "http://det-distribution.dz"
+    },
+    "tags": ["Java", "SpringBoot", "MySQL", "JavaScript"]
+  },
+  {
+    "title": "Liam",
+    "desc": "A portfolio template for professional photographer.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/HTML5-Templates/tree/master/Liam",
+      "Link": "https://liam-iota.vercel.app"
+    },
+    "tags": ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    "title": "Numix Project",
+    "desc": "Recreated the official website for the Numix Project.",
+    "links": {
+      "Source Code": "https://github.com/AM-77/HTML5-Templates/tree/master/numixproject",
+      "Link": "https://numixproject.vercel.app"
+    },
+    "tags": ["HTML", "CSS", "JavaScript"]
+  }
+]
+
+
