ee42b4708f78de669493c426eb1cf36facba9cb1
amine griche <amine.griche77@protonmail.ch>
2024-01-07 14:29:49 +0100

    chore: remove console.log

--- src/routes/api/til/+server.ts
+++ src/routes/api/til/+server.ts
@@ -4,8 +4,6 @@ import { json } from '@sveltejs/kit';
 export const GET = async () => {
 	const allPosts = await loadAllTils();
 
-	console.log({ allPosts });
-
 	const sortedPosts = allPosts.sort((a, b) => {
 		return new Date(b.date).getTime() - new Date(a.date).getTime();
 	});
